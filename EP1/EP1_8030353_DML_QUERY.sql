8cf1aa0827bc7cc71be87aa2d3da9c517794560f
Marcelo Schmitt <marcelo.schmitt1@gmail.com>
2018-05-29 21:14:00 -0300

    MER logico da fase 2
    MER lógico da fase 2 segundo a minha interpretação do que o Décio respondeu no Paca.

--- EP1/EP1_8030353_DML_QUERY.sql
+++ EP1/EP1_8030353_DML_QUERY.sql
@@ -1,3 +1,4 @@
+-- mostra os nomes dos alunos e seus respectivos cursos
 SELECT al_nome, c_nome FROM
   curso CROSS JOIN (SELECT al_nome, c_id FROM
   aluno CROSS JOIN matricula m WHERE m.al_nusp = aluno.al_nusp) t
@@ -18,7 +19,7 @@ SELECT a.al_nome, d.di_nome FROM
 --  JOIN aluno al ON al.al_nusp = c.al_nusp
 --  WHERE c.result = 'A';
 
--- mostra os nomes das disciplinas que cada aluno foi aprovado
+-- mostra os nomes dos alunos e em quais matérias cada um foi aprovado
 SELECT a.al_nome, d.di_nome FROM
   aluno a, disciplina d JOIN caminho c on d.di_sigla = c.di_sigla
   WHERE a.al_nusp = c.al_nusp AND c.result = 'A' ORDER BY a.al_nome;
